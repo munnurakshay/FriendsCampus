95a3487193098677b478aa6b338468b0519d1b77
Akshay Munnur <munnurakshay2004@gmail.com>
2026-03-31 13:10:33 +0530

    Add Firebase Keys Here

--- frontend/admin/admin_dashboard.js
+++ frontend/admin/admin_dashboard.js
@@ -6,13 +6,7 @@ import { getFirestore, collection, getDocs, doc, deleteDoc } from "https://www.g
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyDj2cZJSWL2pVcoZ29fnyLkZZrAltGCeJA",
-  authDomain: "friendscampus-8e333.firebaseapp.com",
-  projectId: "friendscampus-8e333",
-  storageBucket: "friendscampus-8e333.firebasestorage.app",
-  messagingSenderId: "958494452018",
-  appId: "1:958494452018:web:6317b8bea846e029f4cf2b",
-  measurementId: "G-SXX17H0F0T"
+   //Paste The Firebase API KEYS HERE
 };
 
 const app = initializeApp(firebaseConfig);
